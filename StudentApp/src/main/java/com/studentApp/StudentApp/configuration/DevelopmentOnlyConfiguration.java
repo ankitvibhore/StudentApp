eca2bed2cf76f410b6a81da8ccebffbc6bf80a19
Ankit.Vibhore <Ankit.Vibhore@C40-SF---195.india.rsystems.com>
2018-08-14 06:59:26 -0700

    first commit

---
+++ StudentApp/src/main/java/com/studentApp/StudentApp/configuration/DevelopmentOnlyConfiguration.java
@@ -0,0 +1,14 @@
+package com.studentApp.StudentApp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@Profile("dev")
+public class DevelopmentOnlyConfiguration
+{
+	@Bean
+	public String dummy() {
+		return "something";
+	}
+
+}
---
+++ StudentApp/src/main/java/com/studentApp/StudentApp/configuration/ProductionOnlyConfiguration.java
@@ -0,0 +1,14 @@
+package com.studentApp.StudentApp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@Profile("prod")
+public class ProductionOnlyConfiguration
+{
+	@Bean
+	public String dummy() {
+		return "something";
+	}
+
+}
